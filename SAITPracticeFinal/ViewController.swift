bdee4b911751c0db57aef2945f5f95158da51dcf
Sophie <aminsophie@gmail.com>
2018-04-09 13:00:53 -0600

    Initial Commit

---
+++ SAITPracticeFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAITPracticeFinal
+//
+//  Created by Sophia Amin on 4/9/18.
+//  Copyright © 2018 Sophia Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SAITPracticeFinalTests/SAITPracticeFinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  SAITPracticeFinalTests.swift
+//  SAITPracticeFinalTests
+//
+//  Created by Sophia Amin on 4/9/18.
+//  Copyright © 2018 Sophia Amin. All rights reserved.
+//
+
+import XCTest
+@testable import SAITPracticeFinal
+
+class SAITPracticeFinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
